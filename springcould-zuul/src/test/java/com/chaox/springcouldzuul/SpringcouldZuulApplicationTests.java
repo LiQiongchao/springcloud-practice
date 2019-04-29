74b258300f0458303542d5418be134cccba776eb
liqiongchao-oio <liqiongchao2012@163.coom>
2019-04-29 17:18:00 +0800

    eureka 双服务和zuul

---
+++ springcould-zuul/src/test/java/com/chaox/springcouldzuul/SpringcouldZuulApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chaox.springcouldzuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringcouldZuulApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
